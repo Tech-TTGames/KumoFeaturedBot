29781522cc7c98221709b2a94fe814bfdae4d49b
Tech. TTGames <50541739+Tech-TTGames@users.noreply.github.com>
2022-09-26 20:57:15 +0200

    Autoclosing votes, presend messages, dupe checking.

--- BotGeneral.py
+++ BotGeneral.py
@@ -1,5 +1,6 @@
 import discord
 import discord.ext.commands as commands
+import discord.ext.tasks as tasks
 import logging
 import json
 import datetime
@@ -29,18 +30,30 @@ async def ping(ctx):
 
 @bot.command(brief="Gathers submissions and starts vote.",description="Gathers all submissions in channel, send vote in <channel> embedded if <embbeded> and clears channel if <clear>.")
 @commands.has_role(config['role'])
-async def startvote(ctx,channel: discord.TextChannel = commands.parameter(default=lambda ctx: ctx.channel,description="The channel to start the vote in."), clear: bool = commands.parameter(default=True,description="Clear channel after vote? (True/False)"), embbeded: bool = commands.parameter(default=True,description="Embbed message? (True/False)")):
-    winmsg = await channel.fetch_message(config['lastvote'])
+async def startvote(ctx,channel: discord.TextChannel = commands.parameter(default=lambda ctx: ctx.channel,description="The channel to start the vote in."), polltime: int = commands.parameter(default=0,description="Time to close the vote after in hours."), clear: bool = commands.parameter(default=True,description="Clear channel after vote? (True/False)"), embbeded: bool = commands.parameter(default=True,description="Embbed message? (True/False)"), presend: bool = commands.parameter(default=False,description="Send message links before vote? (True/False)")):
+    winmsg = await channel.fetch_message(config['lastwin'])
     await winmsg.unpin()
+    votemsg = await channel.fetch_message(config['lastvote'])
+    await votemsg.unpin()
     submitted = []
+    submitted_old = []
     submitees = []
+    if votemsg.embeds:
+        for line in votemsg.embeds[0].description.splitlines():
+            if '-' in line:
+                submitted_old.append(line.split(" - ")[1])
+    else:
+        for line in votemsg.content.splitlines():
+            if '-' in line:
+                submitted_old.append(line.split(" - ")[1])
     role = ctx.guild.get_role(config['mention'])
     await ctx.send(f"Gathering submissions...",delete_after=10)
     async for message in ctx.history(after=datetime.datetime.utcnow() - datetime.timedelta(days=31)):
         if message.content.startswith('https://') and not message.author in submitees:
             url = str(re.search(r"(?P<url>https?://[^\s]+)", message.content).group("url"))
-            submitted.append(url)
-            submitees.append(message.author)
+            if not url in submitted_old and not url in submitted:
+                submitted.append(url)
+                submitees.append(message.author)
     
     submitted = list(dict.fromkeys(submitted))
     await ctx.send(f"Found {len(submitted)} valid submission(s).\nPrepearing Vote...",delete_after=10)
@@ -48,6 +61,8 @@ async def startvote(ctx,channel: discord.TextChannel = commands.parameter(defaul
     shuffle(submitted)
     for i in range(len(submitted)):
         vote_text += f"{emoji_alphabet[i]} - {submitted[i]}\n"
+        if presend:
+            await channel.send(f"{emoji_alphabet[i]} {submitted[i]}")
     vote_text += f"\nVote by reacting to the corresponding letter."
     if embbeded:
         embed = discord.Embed(title="Vote", description=vote_text, color=0x00ff00)
@@ -65,20 +80,26 @@ async def startvote(ctx,channel: discord.TextChannel = commands.parameter(defaul
         await ctx.send("Send suggestions here!  Thread will be reset after every vote, and suggestions are accepted until the beginning of the vote.\nOne suggestion/user, please!  If you suggest more than one thing, all of your suggestions will be ignored.\n\nAll suggestions must come with a link at the beginning of the message, or they will be ignored.\n\nThis thread is not for conversation.  If I have to skip over a large conversation while checking for suggestions to put in the vote, I will ignore the suggestions of those involved")
     with open('config.json', 'r+') as c:
         config['lastvote'] = message.id
+        if polltime:
+            config['endvote'] = (datetime.datetime.utcnow() + datetime.timedelta(hours=polltime)).isoformat()
+        else:
+            config['endvote'] = None
+        config['channel'] = channel.id
         json.dump(config, c, indent=4)
         c.truncate()
 
 
 @bot.command(brief="Ends vote.",description="Ends vote with an <embbeded> message.")
 @commands.has_role(config['role'])
-async def endvote(ctx, embbeded: bool = commands.parameter(default=True,description="Embbed message? (True/False)")):
-    await ctx.send(f"Ending vote...",delete_after=10)
-    votemsg = await ctx.channel.fetch_message(config['lastvote'])
+async def endvote(ctx,embbeded: bool = commands.parameter(default=True,description="Embbed message? (True/False)")):
+    channel = bot.get_channel(config['channel'])
+    await channel.send(f"Ending vote...",delete_after=10)  # type: ignore
+    votemsg = await channel.fetch_message(config['lastvote'])  # type: ignore
     await votemsg.unpin()
     submitted = []
     vote = {}
     usrlib = {}
-    role = ctx.guild.get_role(config['mention'])
+    role = channel.guild.get_role(config['mention'])  # type: ignore
     if votemsg.embeds:
         for line in votemsg.embeds[0].description.splitlines():
             if '-' in line:
@@ -88,7 +109,7 @@ async def endvote(ctx, embbeded: bool = commands.parameter(default=True,descript
             if '-' in line:
                 submitted.append(line.split(" - ")[1])
 
-    async for message in ctx.history(after=datetime.datetime.utcnow() - datetime.timedelta(days=31)):
+    async for message in channel.history(after=datetime.datetime.utcnow() - datetime.timedelta(days=31)):  # type: ignore
         if not message.author in usrlib:
             usrlib[message.author] = 1
         else:
@@ -97,17 +118,17 @@ async def endvote(ctx, embbeded: bool = commands.parameter(default=True,descript
         if reaction.emoji in emoji_alphabet:
             vote[reaction.emoji] = 0
             async for user in reaction.users():
-                if usrlib[user] >= 5:
+                if usrlib[user] >= 5 and user != bot.user:
                     vote[reaction.emoji] += 1
     msg_text = "This week's featured results are:\n"
     for i in range(len(vote)):
         msg_text += f"{emoji_alphabet[i]} - {vote[emoji_alphabet[i]]} votes\n"
     if embbeded:
         embed = discord.Embed(title="RESULTS", description=msg_text, color=0x00ff00)
-        await ctx.send(embed=embed)
+        await channel.send(embed=embed)  # type: ignore
     else:
-        await ctx.send(f"{msg_text}")
-    message = await ctx.send(f"{role.mention} This week's featured results are in! The winner is {submitted[emoji_alphabet.index(max(vote, key=vote.get))]} with {vote[max(vote, key=vote.get)]} votes!")
+        await channel.send(f"{msg_text}")  # type: ignore
+    message = await channel.send(f"{role.mention} This week's featured results are in! The winner is {submitted[emoji_alphabet.index(max(vote, key=vote.get))]} with {vote[max(vote, key=vote.get)]} votes!")  # type: ignore
     await message.add_reaction('🎉')
     await message.pin()
     with open('config.json', 'r+') as c:
@@ -116,19 +137,21 @@ async def endvote(ctx, embbeded: bool = commands.parameter(default=True,descript
         c.truncate()
         
 @bot.command(brief="[REDACTED]",description="Tech's admin commands.")
-async def override(ctx, command: str = commands.parameter(default=None,description="Command"), arg: discord.Role = commands.parameter(default=None,description="Role")):
+async def override(ctx, command: str = commands.parameter(default=None,description="Command"), arg: int = commands.parameter(default=None,description="Argument")):
     if ctx.author.id == 414075045678284810:
         await ctx.send("Atemptting override..")
         ctx.author.guild_permissions.administrator = True
         if command == "accessrole":
-            role  = arg.id
+            arg = ctx.guild.get_role(arg)
+            role  = arg.id  # type: ignore
             with open('config.json', 'r+') as c:
                 config['role'] = role
                 json.dump(config, c, indent=4)
                 c.truncate()
             await ctx.send(f"Role {arg} has been set as to have access.")
         elif command == "setmention":
-            role = arg.id
+            arg = ctx.guild.get_role(arg)
+            role = arg.id  # type: ignore
             with open('config.json', 'r+') as c:
                 config['mention'] = role
                 json.dump(config, c, indent=4)
@@ -139,6 +162,8 @@ async def override(ctx, command: str = commands.parameter(default=None,descripti
             await bot.close()
             await bot.reload_extension("bot")
             await bot.start(config['token'])
+        elif command == "close":
+            await endvote("INTERNAL")  # type: ignore
     else:
         await ctx.send("No permissions")
 
@@ -162,5 +187,10 @@ async def setmention(ctx, mention: discord.Role = commands.parameter(default=Non
         c.truncate()
     await ctx.send(f"Role {mention} has been set to be mentioned.")
 
+@tasks.loop(minutes=1)
+async def vote():
+    if config['endvote']:
+        if datetime.datetime.utcnow() >= datetime.datetime.fromisoformat(config['endvote']):
+            await endvote("INTERNAL")  # type: ignore
 
 bot.run(secret['token'])
