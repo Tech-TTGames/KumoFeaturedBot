39cb3dd655578a6b267b5ed9eef6102e1b43eec6
Tech. TTGames <50541739+Tech-TTGames@users.noreply.github.com>
2022-09-27 17:48:23 +0200

    Config resolved, pulling

--- BotGeneral.py
+++ BotGeneral.py
@@ -5,6 +5,8 @@ import logging
 import json
 import datetime
 import re
+import git
+import pathlib
 from random import shuffle
 
 with open('secret.json') as f:
@@ -13,6 +15,7 @@ with open('secret.json') as f:
 intents = discord.Intents.default()
 intents.message_content = True
 intents.messages = True
+g = git.cmd.Git(pathlib.Path(__file__).parent.resolve())
 bot = commands.Bot(command_prefix='>', intents=intents)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 emoji_alphabet = ["\U0001F1E6","\U0001F1E7","\U0001F1E8","\U0001F1E9","\U0001F1EA","\U0001F1EB","\U0001F1EC","\U0001F1ED","\U0001F1EE","\U0001F1EF","\U0001F1F0","\U0001F1F1","\U0001F1F2","\U0001F1F3","\U0001F1F4","\U0001F1F5","\U0001F1F6","\U0001F1F7","\U0001F1F8","\U0001F1F9","\U0001F1FA","\U0001F1FB","\U0001F1FC","\U0001F1FD","\U0001F1FE","\U0001F1FF"]
@@ -160,8 +163,11 @@ async def override(ctx, command: str = commands.parameter(default=None,descripti
         elif command == "reboot":
             await ctx.send("Rebooting...")
             await bot.close()
-            await bot.reload_extension("bot")
-            await bot.start(config['token'])
+        elif command == "pull":
+            g.pull()
+            await ctx.send("Pulled.")
+            await ctx.send("Rebooting...")
+            await bot.close()
         elif command == "close":
             await endvote("INTERNAL")  # type: ignore
     else:
