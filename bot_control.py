07b376bb751d1b05e408ac826cbd5deead2cab48
Tech. TTGames <50541739+Tech-TTGames@users.noreply.github.com>
2022-11-17 19:48:23 +0100

    Minor Improvments to bot launch

--- bot_control.py
+++ bot_control.py
@@ -8,14 +8,17 @@ try:
 except FileNotFoundError:
     logging.critical("config.json not found. Entering setup mode.")
     MODE = "setup"
+except json.decoder.JSONDecodeError:
+    logging.critical("config.json could not be read. Entering setup mode.")
+    MODE = "setup"
 
 if __name__ == '__main__':
-    if MODE == "setup":
-        import bot_setup as Botlib
-    elif MODE == "debug":
+    if MODE == "debug":
         import bot_debug as Botlib
     elif MODE == "prod":
         import bot_prod as Botlib
     else:
-        raise Exception("Invalid mode in config.json")
+        import bot_setup as Botlib
+        logging.critical("Invalid or not found mode in config.json. Entering setup mode.")
+        MODE = "setup"
     Botlib.start()
