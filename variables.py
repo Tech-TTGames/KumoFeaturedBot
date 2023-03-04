2e5ee063ea10d543bd6b253b3777a57dad23fffa
Tech. TTGames <50541739+Tech-TTGames@users.noreply.github.com>
2023-03-04 19:53:18 +0100

    Update variables.py

--- variables.py
+++ variables.py
@@ -8,7 +8,7 @@ import discord
 from discord.ext import commands
 
 # v[major].[minor].[release].[build]
-VERSION = "v1.1.0.0"
+VERSION = "v1.1.0.0a"
 EMOJI_ALPHABET = [
     "\U0001F1E6",
     "\U0001F1E7",
